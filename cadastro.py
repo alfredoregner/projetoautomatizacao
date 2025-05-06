339b305623bd01ca742f42b7d44c064ad5e7f484
Matheus Rodrigues <mathrodd132@gmail.com>
2025-05-05 21:20:26 -0300

    att.

--- cadastro.py
+++ cadastro.py
@@ -12,32 +12,44 @@ pagina = planilha['Funcionarios']
 
 # Acessando a linha 2 da planilha escolhida, para que comece a navegar entre as células, coletando as informações preenchidas
 for linha in pagina.iter_rows(min_row = 2):     # Coleta a informação a partir da segunda linha da tabela (a primeira não é necessária nesse caso pois é o cabeçalho do arquivo)
-    # Nome
-    funcionario_nome = linha[0].value       # Acessa o campo selecionado de acordo com o índice ([0] é o primeiro campo da linha 2)
-    pyperclip.copy(funcionario_nome)        # Copia a informação do campo selecionado, com a formatação original
+    # # Nome
+    # funcionario_nome = linha[0].value       # Acessa o campo selecionado de acordo com o índice ([0] é o primeiro campo da linha 2)
+    # pyperclip.copy(funcionario_nome)        # Copia a informação do campo selecionado, com a formatação original
 
-    # Click com o pc do SENAI
-    pyautogui.click(666,431, duration = 1)
+    # # Click com o pc do SENAI
+    # pyautogui.click(666,431, duration = 1)
     
-    # Clickcom o pc do trabalho
-    # pyautogui.click(735,444, duration = 1)  # Clica no local indicado pelas coordenadas
-    pyautogui.hotkey('ctrl', 'v')           # Cola a informação copiada anteriormente, no local selecionado
+    # # Clickcom o pc do trabalho
+    # # pyautogui.click(735,444, duration = 1)  # Clica no local indicado pelas coordenadas
+    # pyautogui.hotkey('ctrl', 'v')           # Cola a informação copiada anteriormente, no local selecionado
     
-    # CPF
-    funcionario_cpf = linha[1].value
-    pyperclip.copy(funcionario_cpf)
+# ID
+    funcionario_id = linha[0].value
+    pyperclip.copy(funcionario_id)
     pyautogui.press('tab', interval = 0.5)  # Troca para o próximo campo
     pyautogui.hotkey('ctrl', 'v')
 
-    # E-mail
-    funcionario_email = linha[2].value
-    pyautogui.press('tab', interval = 0.5)
-    pyperclip.copy(funcionario_email)
+# Nome
+    funcionario_nome = linha[1].value
+    pyperclip.copy(funcionario_nome)
+    pyautogui.press('tab', interval = 0.5)  # Troca para o próximo campo
     pyautogui.hotkey('ctrl', 'v')
 
-    # Telefone
-    funcionario_telefone = linha[3].value
-    pyperclip.copy(funcionario_telefone)
+    # Cargo
+    funcionario_cargo = linha[2].value
+    pyperclip.copy(funcionario_cargo)
+    pyautogui.press('tab', interval = 0.5)  # Troca para o próximo campo
+    pyautogui.hotkey('ctrl', 'v')
+
+    # Departamento
+    funcionario_departamento = linha[3].value
+    pyautogui.press('tab', interval = 0.5)
+    pyperclip.copy(funcionario_departamento)
+    pyautogui.hotkey('ctrl', 'v')
+
+    # Idade
+    funcionario_idade = linha[4].value
+    pyperclip.copy(funcionario_idade)
     pyautogui.press('tab', interval = 0.5)
     pyautogui.hotkey('ctrl', 'v')
 
@@ -46,9 +58,9 @@ for linha in pagina.iter_rows(min_row = 2):     # Coleta a informação a partir
     pyautogui.press('enter', interval = 1)  # Pressiona Enter para seguir para a próxima parte do formulário
     time.sleep(2)
 
-    # CEP
-    funcionario_cep = linha[4].value
-    pyperclip.copy(funcionario_cep)
+    # Data de Nascimento
+    funcionario_data_nascimento = linha[5].value
+    pyperclip.copy(funcionario_data_nascimento)
     # Click com o pc do SENAI
     pyautogui.click(658,445, duration = 1)
     
@@ -56,95 +68,64 @@ for linha in pagina.iter_rows(min_row = 2):     # Coleta a informação a partir
     # pyautogui.click(783,471, duration = 1) 
     pyautogui.hotkey('ctrl', 'v')
 
-    # Rua
-    funcionario_rua = linha[5].value
-    pyperclip.copy(funcionario_rua)
+    # E-mail
+    funcionario_email = linha[6].value
+    pyperclip.copy(funcionario_email)
     pyautogui.press('tab', interval = 0.5)
     pyautogui.hotkey('ctrl', 'v')
 
-    # Número
-    funcionario_numero = linha[6].value
-    pyperclip.copy(funcionario_numero)
+    # Status
+    funcionario_status = linha[7].value
+    pyperclip.copy(funcionario_status)
     pyautogui.press('tab', interval = 0.5)
     pyautogui.hotkey('ctrl', 'v')
 
-    # Complemento
-    funcionario_complemento = linha[7].value
-    pyperclip.copy(funcionario_complemento)
-    pyautogui.press('tab', interval = 0.5)
-    pyautogui.hotkey('ctrl', 'v')
+    # # btn-Próximo
+    # pyautogui.press('tab', interval = 1)
+    # pyautogui.press('tab', interval = 1)
+    # pyautogui.press('enter', interval = 1)
+    # time.sleep(2)
 
-    # Bairro
-    funcionario_bairro = linha[8].value
-    pyperclip.copy(funcionario_bairro)
-    pyautogui.press('tab', interval = 0.5)
-    pyautogui.hotkey('ctrl', 'v')
-
-    # Cidade
-    funcionario_cidade = linha[9].value
-    pyperclip.copy(funcionario_cidade)
-    pyautogui.press('tab', interval = 0.5)
-    pyautogui.hotkey('ctrl', 'v')
-
-    # Estado
+    #     # Clickcom o pc do trabalho
+    #     # pyautogui.click(697,463, duration = 1)
+    # elif funcionario_cargo == 'Supervisor':
+    #     # Click com o pc do SENAI
+    #     pyautogui.click(676,479, duration = 1)
     
-    funcionario_estado = linha[10].value
-    pyperclip.copy(funcionario_estado)
-    pyautogui.press('tab', interval = 0.5)
-    pyautogui.hotkey('ctrl', 'v')
-
-    # btn-Próximo
-    pyautogui.press('tab', interval = 1)
-    pyautogui.press('tab', interval = 1)
-    pyautogui.press('enter', interval = 1)
-    time.sleep(2)
-
-    # Cargo
-    funcionario_cargo = linha[11].value
-    if funcionario_cargo == 'Gerente':         # Verifica o que está escrito na célula e compara com as condições para clicar na opção correta
-        # Click com o pc do SENAI
-        pyautogui.click(676,445, duration = 1)
+    #     # Clickcom o pc do trabalho
+    #     # pyautogui.click(696,503, duration = 1)
+    # elif         funcionario_cargo == 'Repositor':
+    #     # Click com o pc do SENAI
+    #     pyautogui.click(678,519, duration = 1)
     
-        # Clickcom o pc do trabalho
-        # pyautogui.click(697,463, duration = 1)
-    elif funcionario_cargo == 'Supervisor':
-        # Click com o pc do SENAI
-        pyautogui.click(676,479, duration = 1)
+    #     # Clickcom o pc do trabalho
+    #     # pyautogui.click(695,545, duration = 1)
+    # else:
+    #     # Click com o pc do SENAI
+    #     pyautogui.click(675,563, duration = 1)
     
-        # Clickcom o pc do trabalho
-        # pyautogui.click(696,503, duration = 1)
-    elif funcionario_cargo == 'Repositor':
-        # Click com o pc do SENAI
-        pyautogui.click(678,519, duration = 1)
-    
-        # Clickcom o pc do trabalho
-        # pyautogui.click(695,545, duration = 1)
-    else:
-        # Click com o pc do SENAI
-        pyautogui.click(675,563, duration = 1)
-    
-        # Clickcom o pc do trabalho
-        # pyautogui.click(694,583, duration = 1)
+    #     # Clickcom o pc do trabalho
+    #     # pyautogui.click(694,583, duration = 1)
         
-    # Salário
-    funcionario_salario = linha[12].value
-    pyperclip.copy(funcionario_salario)
-    pyautogui.press('tab', interval = 0.5)
-    pyautogui.hotkey('ctrl', 'v')
+    # # Salário
+    # funcionario_salario = linha[12].value
+    # pyperclip.copy(funcionario_salario)
+    # pyautogui.press('tab', interval = 0.5)
+    # pyautogui.hotkey('ctrl', 'v')
 
-    # btn-Enviar
-    pyautogui.press('tab', interval = 1)
-    pyautogui.press('tab', interval = 1)
-    pyautogui.press('enter', interval = 1)
-    time.sleep(2)
+    # # btn-Enviar
+    # pyautogui.press('tab', interval = 1)
+    # pyautogui.press('tab', interval = 1)
+    # pyautogui.press('enter', interval = 1)
+    # time.sleep(2)
 
-    # Enviara outra resposta
-    # Click com o pc do SENAI
-    pyautogui.click(717,246, duration = 1)
+    # # Enviara outra resposta
+    # # Click com o pc do SENAI
+    # pyautogui.click(717,246, duration = 1)
     
     # Clickcom o pc do trabalho
     # pyautogui.click(741,273, duration = 1)
-    time.sleep(2)
+    # time.sleep(2)
 
 # Limpando a tabela após realizar todos os cadastros
 pagina.delete_rows(2, 100)  # Encontrar uma forma de automatizar a identificação da última linha da tabela, para que seja tudo deletado automaticamente a partir da primeira linha com dados na tabela (2), até a última linha com dados preenchidos
