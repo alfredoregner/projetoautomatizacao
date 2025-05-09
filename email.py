6e548d9a3d30d8076018c1a7d6a67f3351937c67
Alfredo Regner <94856772+alfredoregner@users.noreply.github.com>
2025-05-09 00:29:33 -0300

    Exemplo disparo de email
    Adicionado arquivo de exemplo para realizar a função de disparo de e-mail para utilizar no processo de notificação dos funcionários

---
+++ email.py
@@ -0,0 +1,26 @@
+import win32com.client as win32
+
+# instalar: pip install pywin32
+
+# Criar a integração com o Outlook
+outlook = win32.Dispatch('outlook.application')
+
+# Criar um email
+email = outlook.CreateItem(0)
+
+# Configurar as Informações do seu e-mail
+email.To = 'alfredoregner3@gmail.com'
+email.Subject = 'Teste de Disparo de E-mail Automatizado'
+email.HTMLBody = '''
+<p>Olá Alfredo! Aqui é o código Python</p>
+
+<p>O Faturamento da loja foi de R$1500
+Vendemos 10 produtos
+O ticket Médio foi de R$150</p>
+
+<p>Abs,</p>
+<p>Alfredo Tester</P>
+'''
+
+email.Send()
+print("E-mail Enviado com sucesso!")
